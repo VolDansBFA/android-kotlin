e6577def06d9bdf504cb008e6cc23cb057a7dcc8
Nicolas Linard <nicolas.linard@axa.fr>
2018-10-23 21:13:58 +0200

    fix: reset project

---
+++ FlightAchievementTracker/app/src/main/java/fr/axa/flightachievementtracker/Models.kt
@@ -0,0 +1,7 @@
+package fr.axa.flightachivementtracker
+
+import com.google.gson.annotations.SerializedName
+
+data class Character(val name:String, val realm:String, val level:Int, val stats:CharacterStats)
+
+data class CharacterStats (val health: Int, val power:Int, @SerializedName("str") val strength:Int)
---
+++ FlightAchievementTracker/app/src/main/java/fr/axa/flightachievementtracker/WoWAPIService.kt
@@ -0,0 +1,11 @@
+package fr.axa.flightachivementtracker
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface WoWAPIService {
+
+    @GET("wow/character/{realm}/{name}?fields=achievements,reputation,quests,stats,guild&locale=fr_FR&apikey=pkkhjhqfqt9paxx6eezvfd7f6x5hfvee")
+    fun getCharacter(@Path("name") name:String, @Path("realm") realm:String): Call<Character>
+}
