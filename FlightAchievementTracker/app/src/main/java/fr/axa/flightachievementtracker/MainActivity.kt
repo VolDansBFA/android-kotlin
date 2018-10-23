039df1691ee296b83063fc5fbd30604d470db007
Nicolas Linard <nicolas.linard@axa.fr>
2018-10-24 00:14:24 +0200

    feat: added use of realm service and added realm raw model

--- FlightAchievementTracker/app/src/main/java/fr/axa/flightachievementtracker/MainActivity.kt
+++ FlightAchievementTracker/app/src/main/java/fr/axa/flightachievementtracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package fr.axa.flightachievementtracker
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.content_main.*
@@ -10,17 +11,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-class MainActivity : AppCompatActivity(), Callback<Character> {
-    override fun onFailure(call: Call<Character>, t: Throwable) {
-        if (!call.isCanceled) {
-            tv_result.text = "Failed to get Character"
-        }
-    }
-
-    override fun onResponse(call: Call<Character>, response: Response<Character>) {
-        tv_result.text = response.body().toString()
-    }
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,17 +24,45 @@ class MainActivity : AppCompatActivity(), Callback<Character> {
 
         val service:WoWAPIService = retrofit.create(WoWAPIService::class.java)
 
-        val call = service.getCharacter(et_character_name.text.toString(),
+        val characterCall = service.getCharacter(et_character_name.text.toString(),
             et_realm.text.toString(),
             BuildConfig.API_KEY)
 
+        val questCall = service.getQuest("50328", BuildConfig.API_KEY)
+
+        val realmCall = service.getRealms(BuildConfig.API_KEY)
+
+        val achievementCall = service.getAchievement("6", BuildConfig.API_KEY)
+
          b_search.setOnClickListener {
 
-             if (call.isExecuted) {
-                 call.cancel()
+             /*
+             if (characterCall.isExecuted) {
+                 characterCall.cancel()
              } else {
-                 call.enqueue(this)
-             }
+                 characterCall.enqueue(object : Callback<Character> {
+                     override fun onFailure(call: Call<Character>, t: Throwable) {
+                         tv_result.text = "Failed to get Characters"
+                     }
+
+                     override fun onResponse(call: Call<Character>, response: Response<Character>) {
+                         tv_result.text = response.body().toString()
+                     }
+
+                 })
+             }*/
+
+             realmCall.enqueue(object : Callback<Realms> {
+                 override fun onFailure(call: Call<Realms>, t: Throwable) {
+                     tv_result.text = "Failed to get Realms"
+                 }
+
+                 override fun onResponse(call: Call<Realms>, response: Response<Realms>) {
+                     val filteredList : List<Realm>? = response.body()?.realms?.filter { it.locale == "fr_FR" }
+                     tv_result.text = filteredList.toString()
+                 }
+
+             })
         }
     }
 }
--- FlightAchievementTracker/app/src/main/java/fr/axa/flightachievementtracker/Models.kt
+++ FlightAchievementTracker/app/src/main/java/fr/axa/flightachievementtracker/Models.kt
@@ -2,14 +2,28 @@ package fr.axa.flightachievementtracker
 
 import com.google.gson.annotations.SerializedName
 
-data class Character(val name:String, val realm:String, val level:Int, val stats: CharacterStats)
+data class Character(val name:String,
+                     val realm:String,
+                     val level:Int,
+                     val stats: CharacterStats)
 
-data class CharacterStats (val health: Int, val power:Int, @SerializedName("str") val strength:Int)
+data class CharacterStats (val health: Int,
+                           val power:Int,
+                           @SerializedName("str") val strength:Int)
 
 data class Achievement(val name: String)
 
 data class Realms(val realms:List<Realm>)
 
-data class Realm(val name:String)
+data class Realm(val type:String,
+                 val population:String,
+                 val queue:Boolean,
+                 val status: Boolean,
+                 val name: String,
+                 val slug: String,
+                 val battlegroup: String,
+                 val locale:String,
+                 val timezone: String,
+                 val connected_realms: List<String>)
 
 data class Quest(val name: String)
